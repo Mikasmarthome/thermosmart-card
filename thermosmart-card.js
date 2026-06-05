4912f8216328ceb01415df269cab031721801663
Mikasmarthome <278800977+Mikasmarthome@users.noreply.github.com>
2026-06-05 12:25:47 +0200

    chore: bump version to v1.0.1-beta.3

--- thermosmart-card.js
+++ thermosmart-card.js
@@ -1,8 +1,8 @@
 /**
- * ThermoSmart Lovelace Card v1.0.1-beta.1
+ * ThermoSmart Lovelace Card v1.0.1-beta.3
  * https://github.com/Mikasmarthome/thermosmart-card
  */
-const CARD_VERSION = '1.0.1-beta.2';
+const CARD_VERSION = '1.0.1-beta.3';
 
 // ── i18n ─────────────────────────────────────────────────────────────────────
 
